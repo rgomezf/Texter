8e78e9c5043ceedc3a37e62da6e365767fbfa744
Ramon A Gomez <ramon.gomez@noaa.gov>
2016-06-09 10:53:52 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+#Swift sample application - Texter
+
+This is a simple command line Swift App.  It allows the user to send SMS messages
+using the Clock Work API.
+
+#Parameters for running the APP
+
+1. The App will ask for the text to be send.  Just type the content to be send and
+press ENTER.  If the user just press ENTER without entering any content to be
+send the App will exit.
+
+2. The App will ask for the cellphone number which will receive the message.  It
+does the same if the user doesn't enter anything.
+
+
+#Resources
+
+Clock Work API: http://clockworksms.com
+Register for free and in the options for the profile after veryfing the email 
+address search for free credits to send SMS with the API.
--- Texter/main.swift
+++ Texter/main.swift
@@ -1,12 +1,26 @@
-//
-//  main.swift
-//  Texter
-//
-//  Created by Ramon Gomez on 6/8/16.
-//  Copyright © 2016 Ramon Gomez. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("What text do you want to send?")
+guard let userTextMessage = readLine(stripNewline: true) else {exit(0)}
+
+print("Enter the number to whom the text should be send:")
+guard let userPhoneNumber = readLine(stripNewline: true) else {exit(0)}
+
+// Clock Work SMS API Key: 5f8cf06e5652023cfa53e8f9c23a400b87d7a4ef
+
+var clockworkSMSURLString = "https://api.clockworksms.com/http/send.aspx?"
+                        + "key=5f8cf06e5652023cfa53e8f9c23a400b87d7a4ef"
+                        + "&to=" + userPhoneNumber
+                        + "&content=" + userTextMessage
+
+clockworkSMSURLString = clockworkSMSURLString.stringByReplacingOccurrencesOfString(" ", withString: "+")
+
+if let clockworkSMSURL = NSURL(string: clockworkSMSURLString) {
+
+    if let responseData = NSData(contentsOfURL: clockworkSMSURL) {
+        
+        let responseString = String(data: responseData, encoding: NSUTF8StringEncoding)
+            print(responseString)
+    }
+}
 
